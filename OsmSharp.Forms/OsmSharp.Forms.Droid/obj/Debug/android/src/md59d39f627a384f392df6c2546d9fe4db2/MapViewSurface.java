e29b75f154213ea14abd157e5cedf71a33fb8132
Charlenni <info@wfplayer.com>
2016-03-19 10:11:00 +0100

    Initial release

---
+++ OsmSharp.Forms/OsmSharp.Forms.Droid/obj/Debug/android/src/md59d39f627a384f392df6c2546d9fe4db2/MapViewSurface.java
@@ -0,0 +1,126 @@
+package md59d39f627a384f392df6c2546d9fe4db2;
+
+
+public class MapViewSurface
+	extends android.view.View
+	implements
+		mono.android.IGCUserPeer,
+		android.view.ScaleGestureDetector.OnScaleGestureListener,
+		android.view.View.OnTouchListener
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onDraw:(Landroid/graphics/Canvas;)V:GetOnDraw_Landroid_graphics_Canvas_Handler\n" +
+			"n_onLayout:(ZIIII)V:GetOnLayout_ZIIIIHandler\n" +
+			"n_onTouchEvent:(Landroid/view/MotionEvent;)Z:GetOnTouchEvent_Landroid_view_MotionEvent_Handler\n" +
+			"n_onScale:(Landroid/view/ScaleGestureDetector;)Z:GetOnScale_Landroid_view_ScaleGestureDetector_Handler:Android.Views.ScaleGestureDetector/IOnScaleGestureListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onScaleBegin:(Landroid/view/ScaleGestureDetector;)Z:GetOnScaleBegin_Landroid_view_ScaleGestureDetector_Handler:Android.Views.ScaleGestureDetector/IOnScaleGestureListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onScaleEnd:(Landroid/view/ScaleGestureDetector;)V:GetOnScaleEnd_Landroid_view_ScaleGestureDetector_Handler:Android.Views.ScaleGestureDetector/IOnScaleGestureListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onTouch:(Landroid/view/View;Landroid/view/MotionEvent;)Z:GetOnTouch_Landroid_view_View_Landroid_view_MotionEvent_Handler:Android.Views.View/IOnTouchListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("OsmSharp.Android.UI.MapViewSurface, OsmSharp.Android.UI, Version=0.0.0.9999, Culture=neutral, PublicKeyToken=null", MapViewSurface.class, __md_methods);
+	}
+
+
+	public MapViewSurface (android.content.Context p0) throws java.lang.Throwable
+	{
+		super (p0);
+		if (getClass () == MapViewSurface.class)
+			mono.android.TypeManager.Activate ("OsmSharp.Android.UI.MapViewSurface, OsmSharp.Android.UI, Version=0.0.0.9999, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public MapViewSurface (android.content.Context p0, android.util.AttributeSet p1) throws java.lang.Throwable
+	{
+		super (p0, p1);
+		if (getClass () == MapViewSurface.class)
+			mono.android.TypeManager.Activate ("OsmSharp.Android.UI.MapViewSurface, OsmSharp.Android.UI, Version=0.0.0.9999, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public MapViewSurface (android.content.Context p0, android.util.AttributeSet p1, int p2) throws java.lang.Throwable
+	{
+		super (p0, p1, p2);
+		if (getClass () == MapViewSurface.class)
+			mono.android.TypeManager.Activate ("OsmSharp.Android.UI.MapViewSurface, OsmSharp.Android.UI, Version=0.0.0.9999, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public MapViewSurface (android.content.Context p0, android.util.AttributeSet p1, int p2, int p3) throws java.lang.Throwable
+	{
+		super (p0, p1, p2, p3);
+		if (getClass () == MapViewSurface.class)
+			mono.android.TypeManager.Activate ("OsmSharp.Android.UI.MapViewSurface, OsmSharp.Android.UI, Version=0.0.0.9999, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2, p3 });
+	}
+
+
+	public void onDraw (android.graphics.Canvas p0)
+	{
+		n_onDraw (p0);
+	}
+
+	private native void n_onDraw (android.graphics.Canvas p0);
+
+
+	public void onLayout (boolean p0, int p1, int p2, int p3, int p4)
+	{
+		n_onLayout (p0, p1, p2, p3, p4);
+	}
+
+	private native void n_onLayout (boolean p0, int p1, int p2, int p3, int p4);
+
+
+	public boolean onTouchEvent (android.view.MotionEvent p0)
+	{
+		return n_onTouchEvent (p0);
+	}
+
+	private native boolean n_onTouchEvent (android.view.MotionEvent p0);
+
+
+	public boolean onScale (android.view.ScaleGestureDetector p0)
+	{
+		return n_onScale (p0);
+	}
+
+	private native boolean n_onScale (android.view.ScaleGestureDetector p0);
+
+
+	public boolean onScaleBegin (android.view.ScaleGestureDetector p0)
+	{
+		return n_onScaleBegin (p0);
+	}
+
+	private native boolean n_onScaleBegin (android.view.ScaleGestureDetector p0);
+
+
+	public void onScaleEnd (android.view.ScaleGestureDetector p0)
+	{
+		n_onScaleEnd (p0);
+	}
+
+	private native void n_onScaleEnd (android.view.ScaleGestureDetector p0);
+
+
+	public boolean onTouch (android.view.View p0, android.view.MotionEvent p1)
+	{
+		return n_onTouch (p0, p1);
+	}
+
+	private native boolean n_onTouch (android.view.View p0, android.view.MotionEvent p1);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
